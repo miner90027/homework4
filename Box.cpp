3fac9364aaf7161b1a661d6fddb5b667c4bd5a0c
miner90027 <alekszander.mccormick@outlook.com>
2021-03-25 15:01:45 -0800

    Initial commit

---
+++ Box.cpp
@@ -0,0 +1,8 @@
+/* Aleks McCormick
+ * UAF CS 202
+ * Homework 4
+ * Boxes
+ * 03/25/2021
+ */
+
+#include "Box.hpp"
---
+++ Box.hpp
@@ -0,0 +1,17 @@
+/* Aleks McCormick
+ * UAF CS 202
+ * Homework 4
+ * Boxes
+ * 03/25/2021
+ */
+
+#ifndef HOMEWORK4_BOX_HPP
+#define HOMEWORK4_BOX_HPP
+
+
+class Box {
+
+};
+
+
+#endif //HOMEWORK4_BOX_HPP
